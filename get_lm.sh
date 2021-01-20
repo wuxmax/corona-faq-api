df4883b716d3843764f128ff3204891dbc90df0c
reinhard <max.reinhard@dai-labor.de>
2021-01-20 18:07:10 +0100

    fix deploy

--- get_lm.sh
+++ get_lm.sh
@@ -3,8 +3,9 @@
 MODEL_ROOT_URL=https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-transformers/v0.2
 
 MODEL_FILE=distiluse-base-multilingual-cased-v2.zip
+MODEl_NAME=distiluse-base-multilingual-cased-v2
 
 cd language-models
 
 wget "$MODEl_ROOT_URL/$MODEL_FILE"
-unzip $MODEL_FILE
+unzip $MODEL_FILE -d $MODEl_NAME
