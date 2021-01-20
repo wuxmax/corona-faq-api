f452202a089cbe360832ce362b244b22fb5381e4
reinhard <max.reinhard@dai-labor.de>
2021-01-20 17:22:07 +0100

    add model download script

---
+++ get_lm.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+MODEL_ROOT_URL=https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-transformers/v0.2/
+
+MODEL_FILE=distiluse-base-multilingual-cased-v2.zip
+
+wget "$MODEl_ROOT_URL$MODEL_FILE"
+unzip $MODEL_FILE
