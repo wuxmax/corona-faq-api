856c1092a445606cdad6eef7a73a3bfd97349246
reinhard <max.reinhard@dai-labor.de>
2021-01-20 17:23:02 +0100

    add model download script

--- get_lm.sh
+++ get_lm.sh
@@ -4,5 +4,7 @@ MODEL_ROOT_URL=https://public.ukp.informatik.tu-darmstadt.de/reimers/sentence-tr
 
 MODEL_FILE=distiluse-base-multilingual-cased-v2.zip
 
+cd language-models
+
 wget "$MODEl_ROOT_URL$MODEL_FILE"
 unzip $MODEL_FILE
