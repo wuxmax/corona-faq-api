7d44ba68b93d1e01fa744b2eaa8f07b9c31675f3
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2021-03-30 11:23:02 +0200

    Bugfix

--- api/scripts/start-jupyter.sh
+++ api/scripts/start-jupyter.sh
@@ -6,5 +6,5 @@ jupyter notebook \
     --allow-root \
     --ip=0.0.0.0 \
     --no-browser \
-    --notebook-dir="/notebooks" \
+    --notebook-dir="./../notebooks" \
     --NotebookApp.password='sha1:da67d1326b24:37df26a945a0459f12da8d04cb85855387f3332e' &
