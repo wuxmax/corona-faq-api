6980a132171682b3b30dbb9fef33c79983ddc378
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2021-04-03 00:05:41 +0200

    Anpassungen an neue Seite

--- api/app/faq_scraper/sources/th.py
+++ api/app/faq_scraper/sources/th.py
@@ -8,34 +8,43 @@ source_name = "Thüringer Ministerium für Arbeit, Soziales, Gesundheit, Frauen 
 root_url = "https://www.tmasgff.de"
 source_url = root_url + "/covid-19/faq"
 
+
 def get_faq():
+    faqs = []
     html = get_html(source_url)
     soup = BeautifulSoup(html, features="lxml")
-    
-    topic_boxes = soup.find_all("div", {"class": "accordion__wrapper"})
 
-    faqs = [] 
-    
-    for topic_box in topic_boxes:
-        
-        faq_boxes = topic_box.find_all("div", {"class": "accordion__item"})
-                        
-        for faq_box in faq_boxes:
-                        
-            h_question = faq_box.find("h3")
-            div_answer = faq_box.find("div", {"class": "typo-box"})
-        
-            faq = {}
+    topics = soup.find_all("section", class_="module")
+    for topic in topics[2:-1]:
+        url = topic.find("a", class_="button button--small")
+        faqs += get_topic_faq(url['href'])
 
-            faq["q_txt"]: str = h_question.text.strip()
-            faq["a_html"]: str = div_answer.encode_contents()
-            faq["a_txt"]: str = div_answer.text.strip()
-            faq["src_id"] = source_id
-            faq["src_url"]: str = source_url
-            faq["src_name"]: str = source_name
-            faq["id"]: str = question2label(faq["src_id"], faq["q_txt"])
-            faq["nationwide"] = False 
-                        
-            faqs.append(FAQ(**faq))
+    return faqs
+
+
+def get_topic_faq(topic_url: str):
+    html = get_html(root_url + topic_url)
+    soup = BeautifulSoup(html, features="lxml")
+
+    faqs = []
+
+    faq_boxes = soup.find_all("div", {"class": "accordion__item"})
+
+    for faq_box in faq_boxes:
+        h_question = faq_box.find("h3")
+        div_answer = faq_box.find("div", {"class": "typo-box"})
+
+        faq = {}
+
+        faq["q_txt"]: str = h_question.text.strip()
+        faq["a_html"]: str = div_answer.encode_contents()
+        faq["a_txt"]: str = div_answer.text.strip()
+        faq["src_id"] = source_id
+        faq["src_url"]: str = source_url
+        faq["src_name"]: str = source_name
+        faq["id"]: str = question2label(faq["src_id"], faq["q_txt"])
+        faq["nationwide"] = False
+
+        faqs.append(FAQ(**faq))
 
     return faqs
