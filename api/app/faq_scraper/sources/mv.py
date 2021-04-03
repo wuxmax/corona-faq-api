b807080654ae2d5d4fa4f0ccc49fbba12050e468
Olivia Dargel <olivia.dargel@campus.tu-berlin.de>
2021-04-03 02:48:54 +0200

    an neue Seite angepasst

--- api/app/faq_scraper/sources/mv.py
+++ api/app/faq_scraper/sources/mv.py
@@ -1,5 +1,6 @@
-from bs4 import BeautifulSoup
+import re
 
+from bs4 import BeautifulSoup
 from models import FAQ
 from utils import get_html, question2label
 
@@ -8,55 +9,78 @@ source_name = "Ministerpräsidentin des Landes Mecklenburg-Vorpommern - Staatska
 root_url = "https://www.regierung-mv.de"
 source_url = root_url + "/service/Corona-FAQs/"
 
+enumeration_regex = re.compile(r'[0-9]+\. ')
+
+
+def remove_enumeration(string: str):
+    return enumeration_regex.sub("", string)
+
 
 def get_faq():
     html = get_html(source_url)
     soup = BeautifulSoup(html, features="lxml")
-    
+
     faq_ = []
-    
+
     a_topic_ = soup.find_all("a", {"class": "more_blue dvz-contenttype-link"})
-    
+
     for a_topic in a_topic_:
         faq_ += get_topic_faq(a_topic['href'])
-    
+
     return faq_
 
 
 def get_topic_faq(topic_url: str):
     html = get_html(root_url + topic_url)
     soup = BeautifulSoup(html, features="lxml")
-    
-    faq_box = soup.find("div", {"class": "element dvz-contenttype-topiclist element_100 element_index_1"})
-    
-    h_question_ = faq_box.find_all_next("h4")
-    div_answer_ = faq_box.find_all_next("div", {"class": "accordion_content"})
- 
-    try:
-        assert len(h_question_) == len(div_answer_)
-    except AssertionError as e:
-        e.args += ("Number of question elements does not match number of answer elements!", "URL: " + root_url + topic_url)
-        raise
-    
-    faq_ = []
-    
-    for h_question, div_answer in zip(h_question_, div_answer_):
-        
-        # check if answer contains image
-        div_image = div_answer.find("div", {"class": "absatz_image right"})
-        if div_image: div_image.decompose()
-        
-        faq = {}
-        
-        faq["q_txt"]: str = h_question.text
-        faq["a_html"]: str = str(div_answer)
-        faq["a_txt"]: str = div_answer.text.strip()
-        faq["src_id"] = source_id
-        faq["src_url"]: str = root_url + topic_url
-        faq["src_name"]: str = source_name
-        faq["id"]: str = question2label(faq["src_id"], faq["q_txt"])
-        faq["nationwide"] = False
- 
-        faq_.append(FAQ(**faq))
 
-    return faq_
+    faq_box = soup.find("div", {"class": "element dvz-contenttype-topiclist element_100 element_index_1"})
+
+    faqs = []
+
+    first_p_question = faq_box.find("h4")
+
+    p_question = first_p_question
+    elems_answer = []
+
+    for elem in first_p_question.next_siblings:
+        if elem == "\n":
+            elems_answer += [elem]
+
+        elif elem.name == "h4":
+            faq = extract_faq(elems_answer, p_question, topic_url)
+
+            faqs.append(FAQ(**faq))
+
+            p_question = elem
+            elems_answer = []
+
+        else:
+            elems_answer += [elem]
+
+    # add last element
+    if elems_answer != [] and elems_answer != ["\n"]:
+        faq = extract_faq(elems_answer, p_question, topic_url)
+        faqs.append(FAQ(**faq))
+
+    return faqs
+
+
+def extract_faq(elems_answer: list, p_question, topic_url: str):
+    faq = {}
+
+    html_answer = "".join(str(elem) for elem in elems_answer)
+    soup_answer = BeautifulSoup(html_answer, features="lxml")
+
+    faq["q_txt"]: str = remove_enumeration(p_question.text.strip())
+    faq["a_html"]: str = soup_answer.encode_contents()
+    faq["a_txt"]: str = soup_answer.text \
+        .strip() \
+        .replace("Details anzeigen© Ingram Image/adpic© Ingram Image/adpic", "") \
+        .replace("­", "")
+    faq["src_id"] = source_id
+    faq["src_url"]: str = root_url + topic_url
+    faq["src_name"]: str = source_name
+    faq["id"]: str = question2label(faq["src_id"], faq["q_txt"])
+    faq["nationwide"] = False
+    return faq
